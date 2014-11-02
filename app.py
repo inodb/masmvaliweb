51119fc66fb7c80f5a9cb398f448f911443a3a26
Ino de Bruijn <ino.debruijn@scilifelab.se>
2014-11-02 21:03:29 +0100

    Add Celery queue for asm stast calculation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import time
 import errno
 import subprocess
+import settings
+import glob
 
 import flask
 from flask import Flask, request, redirect, url_for
@@ -13,11 +15,52 @@ from IPython.nbformat.current import writes
 from IPython.nbconvert import HTMLExporter
 from IPython.config import Config
 
-UPLOAD_FOLDER = '/tmp/masmvali/'
-ALLOWED_EXTENSIONS = set(['fa', 'fasta'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.from_object(settings)
+ALLOWED_EXTENSIONS = set(['fa', 'fasta'])
+
+
+from celery import Celery
+
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+celery = make_celery(app)
+
+
+@celery.task(name="tasks.add")
+def add_together(a, b):
+    return a + b
+
+
+@celery.task(name="tasks.add_save")
+def add_together_save(a, b, out_file):
+    with open(out_file, 'w') as of:
+        of.write(a + b)
+
+
+@celery.task(name="tasks.run_asm_stats")
+def run_asm_stats(assembly, out_file):
+    os.environ["FASTA_FILE"] = assembly
+    notebook = read(open("masmvaliweb/notebooks/assembly-stats.ipynb"), 'json')
+    r = NotebookRunner(notebook)
+    r.run_notebook()
+    os.remove(assembly)
+    exportHTML = HTMLExporter(config=Config({'HTMLExporter': {'default_template': 'basic'}}))
+    with open(out_file, 'w') as of:
+        of.write(exportHTML.from_notebook_node(r.nb)[0])
 
 
 def make_dir(directory):
@@ -34,23 +77,41 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route("/asmstats", methods=['GET', 'POST'])
+@app.route("/add", methods=['GET'])
+def add_numbers():
+    add_together_save.delay(request.args.get('a'), request.args.get('b'), os.path.join(app.config['UPLOAD_FOLDER'], "test_add.txt"))
+    return "Request sent, result on <a href='/viewadd'>here</a>"
+
+
+@app.route("/viewadd")
+def view_add_numbers():
+    if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], "test_add.txt")):
+        return flask.Response(open(os.path.join(app.config['UPLOAD_FOLDER'], "test_add.txt")).read(), mimetype='text/html')
+    else:
+        return "No result"
+
+
+@app.route("assembly/<asm_name>/notebooks/view/asmstats/")
+def show_notebook(asm_name):
+    fn = os.path.join(app.config["UPLOAD_FOLDER"], asm_name, "asmstats.html")
+    if os.path.isfile(fn):
+        return open(fn).read()
+    else:
+        return "{asm_name} does not have a asm_stats notebook".format(asm_name=asm_name)
+
+
+@app.route("/notebooks/run/asmstats", methods=['GET', 'POST'])
 def asm_stats():
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            make_dir(app.config["UPLOAD_FOLDER"])
-            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(save_path)
-            os.environ["FASTA_FILE"] = save_path
-            notebook = read(open("masmvaliweb/notebooks/assembly-stats.ipynb"), 'json')
-            r = NotebookRunner(notebook)
-            r.run_notebook()
-            os.remove(save_path)
-            exportHTML = HTMLExporter(config=Config({'HTMLExporter': {'default_template': 'basic'}}))
-            #return writes(r.nb, 'json')
-            return exportHTML.from_notebook_node(r.nb)[0]
+        if file and allowed_file(file.filename) and len(request.form['asm_name']) > 0:
+            asm_name = secure_filename(request.form['asm_name'])
+            make_dir(os.path.join(app.config["UPLOAD_FOLDER"], asm_name))
+            save_asm = os.path.join(app.config['UPLOAD_FOLDER'], asm_name, "contigs.fa")
+            file.save(save_asm)
+            out_file = os.path.join(app.config["UPLOAD_FOLDER"], asm_name, "asmstats.html")
+            run_asm_stats(save_asm, out_file)
+            return "Result at <a href='{url}'>{asm_name}</a>".format(asm_name=asm_name, url=url_for('show_notebook', asm_name=asm_name))
     return "Post assembly file"
 
 
@@ -66,8 +127,8 @@ def run_mummer():
 
             def inner():
                 # run MUMmer
-                #mummer_cmd = "echo RUNNING NUCMER && bash -x ~/github/metassemble/scripts/validate/nucmer/run-nucmer.sh test/references/Mircea_07102013_selected_refs.fasta {0} /tmp/nucmer && echo SUCCESS!".format(save_path)
-                mummer_cmd = "echo fakka"
+                mummer_cmd = "echo RUNNING NUCMER && bash -x ~/github/metassemble/scripts/validate/nucmer/run-nucmer.sh test/references/Mircea_07102013_selected_refs.fasta {0} /tmp/nucmer && echo SUCCESS!".format(save_path)
+                #mummer_cmd = "echo fakka"
                 proc = subprocess.Popen(mummer_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
                 while True:
@@ -87,8 +148,8 @@ def run_mummer():
                 r = NotebookRunner(notebook)
                 r.run_notebook()
                 exportHTML = HTMLExporter(config=Config({'HTMLExporter': {'default_template': 'basic'}}))
-                #yield exportHTML.from_notebook_node(r.nb)[0]
-                yield open("masmvaliweb/notebooks/mgcov-comp.html").read()
+                yield exportHTML.from_notebook_node(r.nb)[0]
+                #yield open("masmvaliweb/notebooks/mgcov-comp.html").read()
 
             #return open("/tmp/nucmer.coords").read()
             return flask.Response(inner(), mimetype='text/html')
@@ -107,9 +168,10 @@ def index():
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
-    <form action="/mummer" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
+    <form action="notebooks/run/asmstats" method=post enctype=multipart/form-data>
+      <input type=file name=file><br />
+      Name assembly recipe: <input type="text" name=asm_name><br />
+      <input type=submit value=Upload><br />
     </form>
     <p>%s</p>
     """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
---
+++ settings.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'not_a_secret'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+UPLOAD_FOLDER = '/tmp/masmvali/'
